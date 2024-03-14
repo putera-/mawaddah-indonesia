871b2e5938d83142de50b6ea6cba693c317d69c8
Rizjami Putera <rizjamiputera@gmail.com>
2024-03-14 10:30:30 +0700

    Add class validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Class Validator
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true
+  }));
+
+
   const port = process.env.PORT
   await app.listen(port);
   console.log(`App is running at http://localhost:${port}`)
