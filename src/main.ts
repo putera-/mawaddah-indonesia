fb0ee64bb482640380e1bccde342fdda5f104978
Rizjami Putera <rizjamiputera@gmail.com>
2024-03-19 08:58:05 +0700

    Create end point for avatar file

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,22 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppService } from './app.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  // Class Validator
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true
-  }));
+    // Class Validator
+    app.useGlobalPipes(new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true
+    }));
 
-  const appService = app.get(AppService);
-  await appService.createPath('./public')
-  await appService.createPath('./public/photos')
+    const appService = app.get(AppService);
+    await appService.createPath('./public')
+    await appService.createPath('./public/photos')
+    await appService.createPath('./public/avatar')
 
 
-  const port = process.env.PORT
-  await app.listen(port);
-  console.log(`App is running at http://localhost:${port}`)
+    const port = process.env.PORT
+    await app.listen(port);
+    console.log(`App is running at http://localhost:${port}`)
 }
 bootstrap();
