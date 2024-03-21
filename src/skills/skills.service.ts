46239e8dd8bc3b41a1357e26673c8dcc1bf4d265
mikaazui <adhika725@gmail.com>
2024-03-21 16:52:02 +0700

    update skills

--- src/skills/skills.service.ts
+++ src/skills/skills.service.ts
@@ -1,88 +1,68 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { UpdateSkillDto } from './dto/update-skill.dto';
 import { UsersService } from 'src/users/user.service';
 import { PrismaService } from 'src/prisma.service';
 import { Prisma } from '@prisma/client';
 
+const select = {
+  id: true,
+  userId: true,
+  title: true,
+  createdAt: true,
+  updatedAt: true
+}
 
 @Injectable()
 export class SkillsService {
   constructor(private prisma: PrismaService, private userService: UsersService) { }
 
   async create(id: string, data: Prisma.SkillCreateInput) {
-
-    const user = await this.prisma.user.findUnique({ where: { id } });
-
-    if (!user.id) throw new NotFoundException(`Id not found`);
-
     return this.prisma.skill.create({
-      data: {
-        ...data,
-        User: { connect: { id } }
-      },
-      select: {
-        id: true,
-        userId: true,
-        title: true
-      }
+      data: { ...data, User: { connect: { id } } },
+      select
     });
   }
 
-  findAll(userId: string) {
-    const data = this.prisma.skill.findMany({
-      where: {userId, deleted: false }
-    });
-    if (!data) throw new NotFoundException(`Data not found`);
+  async findAll(userId: string) {
+    const data = await this.prisma.skill.findMany({ where: { userId, deleted: false }, select });
+    if (data.length == 0) throw new NotFoundException(`No Data Found`);
     return data;
-
   }
 
   async findOne(userId: string, id: string) {
+    const data = await this.prisma.skill.findFirst({ where: { id, userId, deleted: false }, select });
+    if (!data) {
+      // Check if the education record exists for any user
+      const skillExist = await this.prisma.skill.findUnique({ where: { id, deleted: false } });
 
-    const data =  this.prisma.skill.findFirst({
-      where: { id, userId , deleted: false }
-    });
-    if (!data) throw new NotFoundException(`Data not found`);
-
-
-    return data
-  }
-
-  async update(userId: string, id : string, data: UpdateSkillDto) {
-    const user = await this.prisma.user.findUnique({ where: { id: userId }, select: { id: true, Skill: true } });
-
-    if (!user) throw new NotFoundException(`user with id ${id} not found`);
-
-    if (!user.Skill.length === null ) throw new NotFoundException(`No skills found for user with id ${id}`);
-
-    const skillId = user.Skill[0].id;
-    return this.prisma.skill.update({
-      where: { id: skillId },
-      data: {
-        ...data,
-        User: { connect: { id: userId } }
-      },
-      select: {
-        id: true,
-        userId: true,
-        title: true
+      // If the education record exists but does not belong to the requesting user
+      if (skillExist) {
+        throw new ForbiddenException(`You dont have permission to access / on this server`);
+      } else {
+        throw new NotFoundException(`Data Not Found`);
       }
-    });
-
+    };
+    return data;
   }
 
-  async remove(userId : string, id: string) {
-    const user = await this.prisma.user.findUnique({ where: { id: userId }, select: { id: true, Skill: true } });
-
-    if (!user) throw new NotFoundException(`user with id ${id} not found`);
-
-    if (!user.Skill.length === null) throw new NotFoundException(`No Married Goals found for user with id ${id}`);
-
-    const skillId = user.Skill[0].id;
+  async update(userId: string, id: string, data: UpdateSkillDto) {
+    const skillId = await this.findOne(userId, id);
 
     return this.prisma.skill.update({
-      where: { id: skillId },
-      data: { deleted: true },
+      where: { id: skillId.id },
+      data: { ...data, User: { connect: { id: userId } } },
+      select
+    });
+  }
+
+  async remove(userId: string, id: string) {
+    const user = await this.prisma.user.findUnique({ where: { id: userId }, select: { id: true, Skill: true } });
+    if (!user.Skill.length === null) throw new NotFoundException(`No data found`);
+    const skillId = await this.findOne(userId, id);
+
+    return this.prisma.skill.update({
+      where: { id: skillId.id },
+      data: { deleted: true }
     });
   }
 }
