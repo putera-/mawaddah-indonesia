aa8b3f3ed447623809427ee0002eb992142054cc
mikaazui <adhika725@gmail.com>
2024-03-21 16:53:58 +0700

    fix province

--- prisma/migrations/20240321085218_update_province_and_biodata/migration.sql
+++ prisma/migrations/20240321085218_update_province_and_biodata/migration.sql
@@ -5,6 +5,7 @@
   - You are about to alter the column `expiredAt` on the `auth` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
   - You are about to drop the column `domicileId` on the `province` table. All the data in the column will be lost.
   - You are about to drop the column `hometownId` on the `province` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[name]` on the table `Province` will be added. If there are existing duplicate values, this will fail.
   - Added the required column `domicile_province` to the `Biodata` table without a default value. This is not possible if the table is not empty.
   - Added the required column `hometown_province` to the `Biodata` table without a default value. This is not possible if the table is not empty.
 
@@ -22,9 +23,13 @@ ALTER TABLE `activation` MODIFY `expiredAt` DATETIME NOT NULL;
 ALTER TABLE `auth` MODIFY `expiredAt` DATETIME NOT NULL;
 
 -- AlterTable
-ALTER TABLE `biodata` ADD COLUMN `domicile_province` VARCHAR(191) NOT NULL,
-    ADD COLUMN `hometown_province` VARCHAR(191) NOT NULL;
+ALTER TABLE `biodata` ADD COLUMN `domicile_province` VARCHAR(100) NOT NULL,
+    ADD COLUMN `hometown_province` VARCHAR(100) NOT NULL;
 
 -- AlterTable
 ALTER TABLE `province` DROP COLUMN `domicileId`,
-    DROP COLUMN `hometownId`;
+    DROP COLUMN `hometownId`,
+    MODIFY `deleted` BOOLEAN NOT NULL DEFAULT false;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `Province_name_key` ON `Province`(`name`);
