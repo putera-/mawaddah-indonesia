ac7bd0a6ae05812b8aaeed7ffb7caa14fd899a34
mikaazui <adhika725@gmail.com>
2024-03-21 16:52:01 +0700

    update prisma

---
+++ prisma/migrations/20240321012128_update/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `expiredAt` on the `activation` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `expiredAt` on the `auth` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+
+*/
+-- AlterTable
+ALTER TABLE `activation` MODIFY `expiredAt` DATETIME NOT NULL;
+
+-- AlterTable
+ALTER TABLE `auth` MODIFY `expiredAt` DATETIME NOT NULL;
