f3cc3507d93fba797b533f76c1020c6e43ecc600
mikaazui <adhika725@gmail.com>
2024-03-21 16:52:02 +0700

    hobbies update

--- src/skills/skills.controller.ts
+++ src/skills/skills.controller.ts
@@ -12,8 +12,9 @@ export class SkillsController {
   @Roles(Role.Member)
   @Post()
   create(@Request() req: any, @Body(new ValidationPipe()) data: CreateSkillDto) {
+    const userId = req.user.id
     try {
-      return this.skillsService.create(req.user.id, data);
+      return this.skillsService.create(userId, data);
 
     } catch (error) {
       throw error;
@@ -35,8 +36,9 @@ export class SkillsController {
   @Roles(Role.Member)
   @Get(':id')
   findOne(@Request() req: any, @Param('id') id: string) {
+    const userId = req.user.id
     try {
-      return this.skillsService.findOne(req.user.id, id);
+      return this.skillsService.findOne(userId, id);
 
     } catch (error) {
       throw error;
@@ -46,8 +48,9 @@ export class SkillsController {
   @Roles(Role.Member)
   @Patch(':id')
   update(@Request() req: any, @Param('id') id: string, @Body(new ValidationPipe()) data: UpdateSkillDto) {
+    const userId = req.user.id
     try {
-      return this.skillsService.update(req.user.id, id, data);
+      return this.skillsService.update(userId, id, data);
 
     } catch (error) {
       throw error;
@@ -59,8 +62,9 @@ export class SkillsController {
   @Delete(':id')
   remove(@Request() req: any, @Param('id') id: string) {
     try {
+      const userId = req.user.id
       if (!id) throw new NotFoundException('Id not found');
-      return this.skillsService.remove(req.user.id, id);
+      return this.skillsService.remove(userId, id);
 
     } catch (error) {
       throw error;
