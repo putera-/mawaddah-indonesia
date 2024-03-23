1c3e2729cbb95beaa145d2b4d7bd03198bb32acf
Brian Adams <albashiroh.programming@gmail.com>
2024-03-23 10:22:31 +0700

    move path profile to auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,6 +9,8 @@ import {
     Post,
     Req,
     Request,
+    UploadedFile,
+    UseInterceptors,
     ValidationPipe,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
@@ -20,11 +22,16 @@ import { Role } from 'src/roles/role.enums';
 import { ChangePasswordDto } from './dto/change-password.dto';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { User } from 'src/users/user.interface';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { UpdateUserDto } from 'src/users/dto/update-user.dto';
+import path from 'path';
+import { PhotosService } from 'src/photos/photos.service';
 @Controller('auth')
 export class AuthController {
     constructor(
         private authService: AuthService,
         private userService: UsersService,
+        private photoService: PhotosService,
     ) {}
     @Public()
     @HttpCode(HttpStatus.OK)
@@ -63,7 +70,68 @@ export class AuthController {
             throw error;
         }
     }
+    @Roles(Role.Superadmin, Role.Admin, Role.Member)
+    @Patch('profile')
+    @UseInterceptors(FileInterceptor('avatar'))
+    async updateUser(
+        @Request() req: any,
+        @Body(new ValidationPipe()) data: UpdateUserDto,
+        @UploadedFile() file: Express.Multer.File,
+    ) {
+        // for avatar
+        const ext = file ? file.originalname.split('.').pop() : '';
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
 
+        // only can update belongs to auth user
+        const { id } = req.user;
+        // prevent change email
+        if (data.email) delete data.email;
+        try {
+            if (file) {
+                const avatarBuffer = file.buffer;
+
+                // resize images to 600, 900, 1200
+                const sizes = [
+                    { key: 'md', size: 900 },
+                    { key: 'lg', size: 1200 },
+                ];
+                await Promise.all(
+                    sizes.map(async (s) => {
+                        const { key, size } = s;
+                        const filename = `${uniqueSuffix}_${key}.${ext}`;
+                        const filepath = path.join(
+                            './public/avatar/' + filename,
+                        );
+
+                        await this.photoService.resize(
+                            size,
+                            avatarBuffer,
+                            filepath,
+                        );
+                    }),
+                );
+
+                data.avatar = `/avatar/${uniqueSuffix}_lg.${ext}`;
+                data.avatar_md = `/avatar/${uniqueSuffix}_md.${ext}`;
+            } else {
+                data.avatar = '';
+                data.avatar_md = '';
+            }
+
+            return this.userService.update(id, data);
+        } catch (error) {
+            // remove avatar
+            if (file) {
+                this.photoService.removeFile(
+                    `/uploads/photos/${uniqueSuffix}_lg.${ext}`,
+                );
+                this.photoService.removeFile(
+                    `/uploads/photos/${uniqueSuffix}_md.${ext}`,
+                );
+            }
+            throw error;
+        }
+    }
     @Get('extend-access-token')
     async extendAccessToken(@Req() req) {
         const user = req.user;
--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,26 +1,17 @@
 import {
     Controller,
     Get,
-    Post,
     Body,
     Patch,
     Param,
     Delete,
     HttpCode,
-    ValidationPipe,
     Request,
-    UseInterceptors,
-    UploadedFile,
     Query,
 } from '@nestjs/common';
 import { UsersService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Prisma, RoleStatus } from '@prisma/client';
-import { User } from './user.interface';
-import { Public } from 'src/auth/auth.metadata';
-import { FileInterceptor } from '@nestjs/platform-express';
-import path from 'path';
 import { PhotosService } from 'src/photos/photos.service';
 import { Roles } from 'src/roles/roles.decorator';
 import { Role } from 'src/roles/role.enums';
@@ -55,68 +46,6 @@ export class UsersController {
         }
     }
 
-    @Roles(Role.Superadmin, Role.Admin, Role.Member)
-    @Patch()
-    @UseInterceptors(FileInterceptor('avatar'))
-    async updateUser(
-        @Request() req: any,
-        @Body(new ValidationPipe()) data: UpdateUserDto,
-        @UploadedFile() file: Express.Multer.File,
-    ) {
-        // for avatar
-        const ext = file ? file.originalname.split('.').pop() : '';
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-
-        // only can update belongs to auth user
-        const { id } = req.user;
-        // prevent change email
-        if (data.email) delete data.email;
-        try {
-            if (file) {
-                const avatarBuffer = file.buffer;
-
-                // resize images to 600, 900, 1200
-                const sizes = [
-                    { key: 'md', size: 900 },
-                    { key: 'lg', size: 1200 },
-                ];
-                await Promise.all(
-                    sizes.map(async (s) => {
-                        const { key, size } = s;
-                        const filename = `${uniqueSuffix}_${key}.${ext}`;
-                        const filepath = path.join(
-                            './public/avatar/' + filename,
-                        );
-
-                        await this.photoService.resize(
-                            size,
-                            avatarBuffer,
-                            filepath,
-                        );
-                    }),
-                );
-
-                data.avatar = `/avatar/${uniqueSuffix}_lg.${ext}`;
-                data.avatar_md = `/avatar/${uniqueSuffix}_md.${ext}`;
-            } else {
-                data.avatar = '';
-                data.avatar_md = '';
-            }
-
-            return this.userService.update(id, data);
-        } catch (error) {
-            // remove avatar
-            if (file) {
-                this.photoService.removeFile(
-                    `/uploads/photos/${uniqueSuffix}_lg.${ext}`,
-                );
-                this.photoService.removeFile(
-                    `/uploads/photos/${uniqueSuffix}_md.${ext}`,
-                );
-            }
-            throw error;
-        }
-    }
     @Roles(Role.Superadmin, Role.Admin, Role.Member)
     @Patch(':id')
     update(@Param('id') id: string, @Body() data: UpdateUserDto) {
