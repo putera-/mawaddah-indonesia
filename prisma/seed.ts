ac47d709132cb0eb28db4d5ebe11af03d2e4e544
mikaazui <adhika725@gmail.com>
2024-03-23 08:34:40 +0700

    update seed > hapus province

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,7 +2,6 @@ import { PrismaClient } from '@prisma/client';
 import { clientSeed } from './seed/client';
 import { userSeed } from './seed/user';
 import { gallerySeed } from './seed/gallery';
-import { provinceSeed } from './seed/province';
 import { memberSeed } from './seed/member';
 import { sliderSeed } from './seed/slider';
 import { faqSeed } from './seed/faq';
@@ -10,11 +9,9 @@ const prisma = new PrismaClient();
 
 async function main() {
     const clientid: string = '94de0914-cf51-47a4-8234-812824d9848a';
-    const userId: string = '0766a4ea-ac7a-49b1-acca-49f3a4c24648';
     await clientSeed(prisma, clientid);
     await userSeed(prisma);
     await gallerySeed(prisma, clientid);
-    await provinceSeed(prisma);
     await memberSeed(prisma);
     await sliderSeed(prisma, clientid);
     await faqSeed(prisma, clientid);
