63f102e43a60ab2653942366d033ac3006e4ae60
mikaazui <adhika725@gmail.com>
2024-03-24 21:35:36 +0700

    update hobbies with pagination

--- src/hobbies/hobbies.service.ts
+++ src/hobbies/hobbies.service.ts
@@ -1,85 +1,103 @@
-import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+    ForbiddenException,
+    Injectable,
+    NotFoundException,
+} from '@nestjs/common';
 import { UpdateHobbyDto } from './dto/update-hobby.dto';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 import { UsersService } from 'src/users/user.service';
 
 const select = {
-  id: true,
-  userId: true,
-  title: true,
-  createdAt: true,
-  updatedAt: true
-}
+    id: true,
+    userId: true,
+    title: true,
+    createdAt: true,
+    updatedAt: true,
+};
 
 @Injectable()
 export class HobbiesService {
-  constructor(private prisma: PrismaService, private userService: UsersService) { }
+    constructor(
+        private prisma: PrismaService,
+        private userService: UsersService,
+    ) {}
 
-  async create(id: string, data: Prisma.HobbyCreateInput) {
-    return this.prisma.hobby.create({
-      data: { ...data, User: { connect: { id } } },
-      select
-    });
-  }
-
-  async findAll(userId: string, page: number = 1, limit: number = 10) {
-    const skip = (page - 1) * limit;
-    const [total, data] = await Promise.all([
-      this.prisma.hobby.count({
-        where: { userId, deleted: false },
-      }),
-      this.prisma.hobby.findMany({
-        where: { userId, deleted: false },
-        orderBy: { createdAt: 'desc' },
-        select,
-        skip,
-        take: Number(limit),
-      }),
-    ]);
-    return {
-      data,
-      total,
-      page: +page,
-      maxPages: Math.ceil(total / limit),
-      limit: +limit
+    async create(id: string, data: Prisma.HobbyCreateInput) {
+        return this.prisma.hobby.create({
+            data: { ...data, User: { connect: { id } } },
+            select,
+        });
     }
-  }
 
-  async findOne(userId: string, id: string) {
-    const data = await this.prisma.hobby.findFirst({ where: { id, userId, deleted: false }, select });
-    if (!data) {
-      // Check if the education record exists for any user
-      const hobbyExist = await this.prisma.hobby.findUnique({ where: { id, deleted: false } });
+    async findAll(userId: string, page: number = 1, limit: number = 10) {
+        const skip = (page - 1) * limit;
+        const [total, data] = await Promise.all([
+            this.prisma.hobby.count({
+                where: { userId, deleted: false },
+            }),
+            this.prisma.hobby.findMany({
+                where: { userId, deleted: false },
+                orderBy: { createdAt: 'desc' },
+                select,
+                skip,
+                take: Number(limit),
+            }),
+        ]);
+        return {
+            data,
+            total,
+            page: +page,
+            maxPages: Math.ceil(total / limit),
+            limit: +limit,
+        };
+    }
 
-      // If the education record exists but does not belong to the requesting user
-      if (hobbyExist) {
-        throw new ForbiddenException(`You dont have permission to access / on this server`);
-      } else {
-        throw new NotFoundException(`Data Not Found`);
-      }
-    };
-    return data;
-  }
+    async findOne(userId: string, id: string) {
+        const data = await this.prisma.hobby.findFirst({
+            where: { id, userId, deleted: false },
+            select,
+        });
+        if (!data) {
+            // Check if the education record exists for any user
+            const hobbyExist = await this.prisma.hobby.findUnique({
+                where: { id, deleted: false },
+            });
 
-  async update(userId: string, id: string, data: UpdateHobbyDto) {
-    const hobbyId = await this.findOne(userId, id);
+            // If the education record exists but does not belong to the requesting user
+            if (hobbyExist) {
+                throw new ForbiddenException(
+                    `You dont have permission to access / on this server`,
+                );
+            } else {
+                throw new NotFoundException(`Data Not Found`);
+            }
+        }
+        return data;
+    }
 
-    return this.prisma.hobby.update({
-      where: { id: hobbyId.id },
-      data: { ...data, User: { connect: { id: userId } } },
-      select
-    });
-  }
+    async update(userId: string, id: string, data: UpdateHobbyDto) {
+        const hobbyId = await this.findOne(userId, id);
 
-  async remove(userId: string, id: string) {
-    const user = await this.prisma.user.findUnique({ where: { id: userId }, select: { id: true, Hobby: true } });
-    if (!user.Hobby.length === null) throw new NotFoundException(`No data found`);
-    const hobbyId = await this.findOne(userId, id);
+        return this.prisma.hobby.update({
+            where: { id: hobbyId.id },
+            data: { ...data, User: { connect: { id: userId } } },
+            select,
+        });
+    }
 
-    return this.prisma.hobby.update({
-      where: { id: hobbyId.id },
-      data: { deleted: true }
-    });
-  }
+    async remove(userId: string, id: string) {
+        const user = await this.prisma.user.findUnique({
+            where: { id: userId },
+            select: { id: true, Hobby: true },
+        });
+        if (!user.Hobby.length === null)
+            throw new NotFoundException(`No data found`);
+        const hobbyId = await this.findOne(userId, id);
+
+        return this.prisma.hobby.update({
+            where: { id: hobbyId.id },
+            data: { deleted: true },
+        });
+    }
 }
