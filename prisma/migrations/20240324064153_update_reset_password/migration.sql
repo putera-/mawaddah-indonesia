ab3ce93e5198c399bde8061e3cae539539f0cd85
mikaazui <adhika725@gmail.com>
2024-03-24 14:05:05 +0700

    finished creating reset password (send email pending)

---
+++ prisma/migrations/20240324064153_update_reset_password/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `expiredAt` on the `activation` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `expiredAt` on the `auth` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `expiredAt` on the `reset_password` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+
+*/
+-- DropIndex
+DROP INDEX `Reset_password_userEmail_key` ON `reset_password`;
+
+-- AlterTable
+ALTER TABLE `activation` MODIFY `expiredAt` DATETIME NOT NULL;
+
+-- AlterTable
+ALTER TABLE `auth` MODIFY `expiredAt` DATETIME NOT NULL;
+
+-- AlterTable
+ALTER TABLE `reset_password` MODIFY `expiredAt` DATETIME NOT NULL;
--- src/reset_password/reset_password.controller.ts
+++ src/reset_password/reset_password.controller.ts
@@ -6,42 +6,43 @@ import { ChangePasswordDto } from 'src/auth/dto/change-password.dto';
 
 @Controller('reset-password')
 export class ResetPasswordController {
-  constructor(private readonly resetPasswordService: ResetPasswordService,) { }
+    constructor(private readonly resetPasswordService: ResetPasswordService,) { }
 
-  @Post()
-  create(@Request() token: string, @Body('userEmail') email: string, data: CreateResetPasswordDto) {
-    try {
-      return this.resetPasswordService.create(token, email, data);
+    @Post()
+    create(@Request() token: string, @Body('userEmail') email: string, data: CreateResetPasswordDto) {
+        try {
+            return this.resetPasswordService.create(token, email, data);
 
-    } catch (error) {
-      throw error
+        } catch (error) {
+            throw error
+        }
     }
-  }
 
-  @Patch(':id')
-  update(@Request() req: any, @Param('id') id: string, @Body() data: ChangePasswordDto) {
-    try {
-      return this.resetPasswordService.update(req.token, id, data);
+    @Patch(':id')
+    update(@Body('token') token: string, @Param('id') id: string, @Body() data: ChangePasswordDto) {
+        try {
+            // console.log(req.user.userEmail)
+            return this.resetPasswordService.update(token, id, data);
 
-    } catch (error) {
-      throw error;
+        } catch (error) {
+            throw error;
+        }
     }
-  }
 
-  // @Get()
-  // findAll() {
-  //   return this.resetPasswordService.findAll();
-  // }
+    // @Get()
+    // findAll() {
+    //   return this.resetPasswordService.findAll();
+    // }
 
-  // @Get(':id')  
-  // findOne(@Param('id') id: string) {
-  //   return this.resetPasswordService.findOne(+id);
-  // }
-
+    // @Get(':id')
+    // findOne(@Param('id') id: string) {
+    //   return this.resetPasswordService.findOne(+id);
+    // }
 
 
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.resetPasswordService.remove(+id);
-  // }
+
+    // @Delete(':id')
+    // remove(@Param('id') id: string) {
+    //   return this.resetPasswordService.remove(+id);
+    // }
 }
