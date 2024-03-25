3f2674587a95a918a3b8470a601cc7d4fef905ad
Brian Adams <albashiroh.programming@gmail.com>
2024-03-25 10:44:36 +0700

    province seed

--- src/candidate/candidate.module.ts
+++ src/candidate/candidate.module.ts
@@ -4,9 +4,16 @@ import { CandidateController } from './candidate.controller';
 import { PrismaService } from 'src/prisma.service';
 import { UsersService } from 'src/users/user.service';
 import { AppService } from 'src/app.service';
+import { BiodataService } from 'src/biodata/biodata.service';
 
 @Module({
     controllers: [CandidateController],
-    providers: [CandidateService, PrismaService, UsersService, AppService],
+    providers: [
+        CandidateService,
+        PrismaService,
+        UsersService,
+        AppService,
+        BiodataService,
+    ],
 })
 export class CandidateModule {}
