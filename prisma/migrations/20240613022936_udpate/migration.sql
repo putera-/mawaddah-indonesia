03b584f5aa31974962e92c62163c4e193226a8ed
mikaazui <adhika725@gmail.com>
2024-06-13 10:15:01 +0700

    finished non_physical_characters

---
+++ prisma/migrations/20240613022936_udpate/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE `nonphysicalcharacter` MODIFY `motto` TEXT NULL,
+    MODIFY `life_goal` TEXT NULL,
+    MODIFY `hobby` TEXT NULL,
+    MODIFY `spare_time_activity` TEXT NULL,
+    MODIFY `positive_traits` TEXT NULL,
+    MODIFY `negative_traits` TEXT NULL,
+    MODIFY `liked_things` TEXT NULL,
+    MODIFY `unliked_things` TEXT NULL;
