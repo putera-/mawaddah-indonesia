e64cf36ddba4bcfac04b9143716b0ae77231fc4a
Rizjami Putera <rizjamiputera@gmail.com>
2024-07-26 11:16:46 +0700

    migrate: user data with password & biodata

---
+++ script/migrate.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+import mysql from 'mysql2/promise'
+import { user } from './data/user';
+
+const new_db = new PrismaClient();
+
+// Configure your old database connection
+const oldDbConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'mawaddah',
+};
+
+let old_db: mysql.Connection;
+
+async function main() {
+    old_db = await mysql.createConnection(oldDbConfig);
+
+    await user(old_db, new_db)
+
+
+}
+
+main()
+    .then(async () => {
+        await new_db.$disconnect();
+        await old_db.end();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await new_db.$disconnect();
+        await old_db.end();
+        process.exit(1);
+    });
