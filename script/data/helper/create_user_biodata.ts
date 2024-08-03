82c125660b89a3949152783aa86715920de47e0c
mikaazui <adhika725@gmail.com>
2024-08-03 10:04:40 +0700

    done migration gambaran_keluarga

---
+++ script/data/helper/create_user_biodata.ts
@@ -0,0 +1,49 @@
+import { faker } from "@faker-js/faker";
+import { Gender, ManhajStatus, MarriagePermission, MarriageStatus, Prisma, PrismaClient } from "@prisma/client";
+
+export async function create_dummy_user_biodata(old_id: number, new_db: PrismaClient, index = 0) {
+    const firstname = faker.person.firstName('male') + index;
+    const lastname = faker.person.lastName('male');
+    const email = faker.internet.email({ firstName: firstname, lastName: lastname }).toLowerCase();
+    const data_user: Prisma.UserCreateInput = {
+        firstname,
+        lastname,
+        email,
+        active: true,
+        verified: true,
+        backup_detail: {
+            create: {
+                old_id
+            }
+        },
+        biodata: {
+            create: {
+                bio: "-",
+                phone: "08112345678",
+                dob: new Date().toISOString(),
+                birth_order: 1,
+                birth_place: "Jakarta",
+                address: "-",
+                address_town: "-",
+                address_province: "-",
+                address_zip_code: 0,
+                hometown_province: "-",
+                poligami_opinion: "-",
+                ethnic: "cina",
+                manhaj: ManhajStatus.NON_SALAF,
+                gender: Gender.PRIA,
+                marriage_status: MarriageStatus.LAJANG,
+                marriage_permission: MarriagePermission.NON_POLIGAMI,
+            }
+        }
+    }
+
+    const user = await new_db.user.create({
+        data: data_user,
+        include: {
+            backup_detail: true
+        }
+
+    });
+    return user;
+}
