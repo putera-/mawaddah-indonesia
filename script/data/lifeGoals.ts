231b136eaf8ef2aad94343d614e127c75a709f03
mikaazui <adhika725@gmail.com>
2024-08-06 10:01:50 +0700

    creating harapam (incomplete)

--- script/data/lifeGoals.ts
+++ script/data/lifeGoals.ts
@@ -1,69 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+import { LifeGoal, Prisma, PrismaClient } from "@prisma/client";
 import mysql from 'mysql2/promise'
-
-
-const tujuanHidup = [
-    {
-        title: 'Saya ingin mencapai keseimbangan antara kehidupan kerja dan pribadi yang harmonis',
-    },
-    {
-        title: 'Saya berharap bisa menjalani kehidupan dengan penuh rasa syukur dan kebahagiaan',
-    },
-    {
-        title: 'Saya bertekad untuk terus belajar dan mengembangkan diri sepanjang hidup',
-    },
-    {
-        title: 'Saya ingin berkontribusi positif bagi masyarakat dan membantu mereka yang membutuhkan',
-    },
-    {
-        title: 'Saya berharap bisa menjalani hidup dengan penuh kesehatan dan energi',
-    },
-    {
-        title: 'Saya ingin membangun hubungan yang bermakna dan mendalam dengan keluarga dan teman',
-    },
-    {
-        title: 'Saya berencana untuk mengeksplorasi dan memahami berbagai budaya dunia melalui perjalanan',
-    },
-    {
-        title: 'Saya berharap bisa mencapai stabilitas finansial dan hidup tanpa beban hutang',
-    },
-    {
-        title: 'Saya bertekad untuk mengejar dan mewujudkan impian karier saya',
-    },
-    {
-        title: 'Saya ingin menciptakan sesuatu yang memiliki dampak positif dan tahan lama',
-    },
-    {
-        title: 'Saya berharap bisa hidup secara mandiri dan otonom'
-    },
-    {
-        title: 'Saya ingin membangun keluarga yang bahagia dan harmonis'
-    },
-    {
-        title: 'Saya bertekad untuk menjaga lingkungan dan menjalani gaya hidup yang ramah lingkungan',
-    },
-    {
-        title: 'Saya berharap bisa menjadi sumber inspirasi dan motivasi bagi orang lain',
-    },
-    {
-        title: 'Saya ingin mengejar passion saya dan menjalani hidup dengan penuh semangat',
-    },
-    {
-        title: 'Saya berharap bisa menikmati setiap momen dalam hidup dengan penuh kesadaran dan kehadiran',
-    },
-    {
-        title: 'Saya berkomitmen untuk menjaga kesehatan mental dan emosional saya',
-    },
-    {
-        title: 'Saya ingin terus mengejar pendidikan dan memperoleh pengetahuan baru',
-    },
-    {
-        title: 'Saya berharap bisa mencapai perdamaian batin dan kesejahteraan spiritual',
-    },
-    {
-        title: 'Saya ingin mengembangkan keterampilan baru dan terus tumbuh sebagai individu',
-    },
-];
+import { create_dummy_user_biodata } from "./helper/create_user_biodata";
 
 const parameters = process.argv;
 
@@ -73,13 +10,105 @@ const parameters = process.argv;
 const isTest = parameters.includes('test');
 
 /*
-migrasi table gazwah.gambaran_keluarga > rq.family_member
+migrasi table gazwah.harapan > rq.;ifeGoals
 */
 export async function harapan(old_db: mysql.Connection, new_db: PrismaClient) {
-    const [lifeGoals]: any[] = await old_db.execute("SELECT * FROM gambaran_keluarga");
+    const [lifeGoals]: any[] = await old_db.execute("SELECT * FROM harapan");
 
     for (let i = 0; i < lifeGoals.length; i++) {
+        const goal = lifeGoals[i];
+        const old_user_id = goal.user_id;
+        process.stdout.write('.');
+        let backup_detail = await new_db.backupDetail.findFirst({
+            where: {
+                old_id: old_user_id
+            }
+        })
+        if (backup_detail == null && isTest) {
+            //create data dummy
+            const user = await create_dummy_user_biodata(old_user_id, new_db, i);
+            backup_detail = user.backup_detail
+        }
+        if (backup_detail != null) {
+            const biodata = await new_db.biodata.findFirst({
+                where: {
+                    userId: backup_detail.userId
+                }
+            })
+
+            //TODO bikin schema 'Sumber dana acara pernikahan'
+
+            //  if (goal.tag == 'Sumber dana acara pernikahan') {
+            //     return dataLifeGoals.career
+            // }
+            let child_count = ''
+            if (goal.tag == 'Keturunan dan pendidikan anak') {
+                child_count = goal.keterangan
+            }
+
+            let career = ''
+            if (goal.tag == 'Karier ke depan' || goal.tag == 'Rencana Karier ke depan?') {
+                career = goal.keterangan
+            }
+
+            let domicile = ''
+            if (goal.tag == 'Domisili ke depan?' || goal.tag == 'Domisili') {
+                domicile = goal.keterangan
+            }
+
+            let child_education = ''
+            if (goal.tag == 'Pendidikan Anak?' || goal.tag == 'Keturunan dan pendidikan anak') {
+                child_education = goal.keterangan
+            }
+
+            let financial_arrangement = ''
+            if (goal.tag == 'Pengelolaan keuangan rumah tangga' || goal.tag == 'Keuangan Rumah Tangga?') {
+                financial_arrangement = goal.keterangan
+            }
+
+            let knowledge_upgrade = ''
+            if (goal.tag == 'Peningkatan ilmu agama' || goal.tag == 'Peningkatan ilmu agama?') {
+                knowledge_upgrade = goal.keterangan
+            }
+
+            let short_term_achievement = ''
+            if (goal.tag == 'Target Jangka Pendek') {
+                short_term_achievement = goal.keterangan
+            }
+
+            let long_term_achievement = ''
+            if (goal.tag == 'Target jangka panjang') {
+                long_term_achievement = goal.keterangan
+            }
+
+            let wife_work_permit_desc = ''
+            if (goal.tag == 'Istri Boleh bekerja?' || goal.tag == 'Memperbolehkan istri untuk bekerja') {
+                wife_work_permit_desc = goal.keterangan
+            }
+
+            const dataLifeGoals = {
+                career,
+                domicile,
+                child_count,
+                child_education,
+                financial_arrangement,
+                knowledge_upgrade,
+                short_term_achievement,
+                long_term_achievement,
+                wife_work_permit_desc,
+            }
+
+
+            const new_life_goals: Prisma.LifeGoalCreateInput = {
+                ...dataLifeGoals,
+                biodata: { connect: { id: biodata.id } }
+            }
+
+            await new_db.lifeGoal.create({ data: new_life_goals })
+
+            console.log(new_life_goals)
+
 
         }
     }
-
+}
--- script/migrate.ts
+++ script/migrate.ts
@@ -3,6 +3,7 @@ import mysql from 'mysql2/promise';
 import { user } from './data/user';
 import { gambaran_keluarga } from './data/familyMember';
 import { biodata } from './data/biodata';
+import { harapan } from './data/lifeGoals';
 
 const new_db = new PrismaClient();
 
@@ -24,10 +25,11 @@ let old_db: mysql.Connection;
 async function main() {
     old_db = await mysql.createConnection(oldDbConfig);
 
-    await user(old_db, new_db);
+    // await user(old_db, new_db);
     await biodata(old_db, new_db);
-    await gambaran_keluarga(old_db, new_db)
+    // await gambaran_keluarga(old_db, new_db)
 
+    await harapan(old_db, new_db);
 }
 
 main()
