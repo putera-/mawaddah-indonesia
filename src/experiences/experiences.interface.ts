b01e7de40e4d436856e31d484751c05078292e49
mikaazui <adhika725@gmail.com>
2024-08-19 09:59:44 +0700

    in progress of making 'experiences' api

---
+++ src/experiences/experiences.interface.ts
@@ -0,0 +1,17 @@
+import { Biodata, ExperienceType } from "@prisma/client"
+
+export interface Experiences {
+    id: string
+    biodata: Biodata
+    biodataId: string
+    type: ExperienceType
+    start_year:number
+    end_year:number
+    position:string
+    description:string
+    createdAt: Date
+    updatedAt: Date
+}
+
+
+
