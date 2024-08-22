3cb3a811d19e7e76a336cbb552230346cbbbfb93
mikaazui <adhika725@gmail.com>
2024-08-22 11:46:44 +0700

    done experience

--- src/experiences/experiences.controller.ts
+++ src/experiences/experiences.controller.ts
@@ -1,34 +1,108 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Request, ValidationPipe, Query, HttpCode, NotFoundException } from '@nestjs/common';
 import { ExperiencesService } from './experiences.service';
 import { CreateExperienceDto } from './dto/create-experience.dto';
 import { UpdateExperienceDto } from './dto/update-experience.dto';
+import { Roles } from 'src/roles/roles.decorator';
+import { Role } from 'src/roles/role.enums';
+import { Prisma } from '@prisma/client';
+import { BiodataService } from 'src/biodata/biodata.service';
 
 @Controller('experiences')
 export class ExperiencesController {
-  constructor(private readonly experiencesService: ExperiencesService) {}
+    constructor(private readonly experienceService: ExperiencesService,
+        private readonly biodataService: BiodataService
+    ) { }
 
-  @Post()
-  create(@Body() createExperienceDto: CreateExperienceDto) {
-    return this.experiencesService.create(createExperienceDto);
-  }
+    @Roles(Role.Member)
+    @Post()
+    async create(@Request() req: any, @Body(new ValidationPipe()) data: CreateExperienceDto) {
+        const userId = req.user.id
+        try {
 
-  @Get()
-  findAll() {
-    return this.experiencesService.findAll();
-  }
+            const biodata = await this.biodataService.findMe(userId)
+            const experience: Prisma.ExperienceCreateInput = {
+                ...data,
+                biodata: {
+                    connect: { id: biodata.id }
+                }
+            }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.experiencesService.findOne(+id);
-  }
+            console.log(experience)
+            return this.experienceService.create(experience);
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateExperienceDto: UpdateExperienceDto) {
-    return this.experiencesService.update(+id, updateExperienceDto);
-  }
+        } catch (error) {
+            throw error;
+        }
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.experiencesService.remove(+id);
-  }
+    @Roles(Role.Member)
+    @Get()
+    async findAll(@Request() req: any, @Query('page') page: number, @Query('limit') limit: number) {
+        const userId = req.user.id
+        try {
+            const biodata = await this.biodataService.findMe(userId)
+
+            if (!biodata) throw new NotFoundException('Biodata not found');
+
+            return this.experienceService.findAll(biodata.id, page, limit);
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+
+    @Roles(Role.Member)
+    @Get(':id')
+    async findOne(@Request() req: any, @Param('id') id: string) {
+        const userId = req.user.id
+        try {
+            const biodata = await this.biodataService.findMe(userId)
+
+            if (!biodata) throw new NotFoundException('Biodata not found');
+
+            return this.experienceService.findOne(biodata.id, id);
+
+        } catch (error) {
+            throw error;
+
+        }
+    }
+
+    @Roles(Role.Member)
+    @Patch(':id')
+    async update(@Request() req: any, @Param('id') id: string, @Body(new ValidationPipe()) data: UpdateExperienceDto) {
+        const userId = req.user.id
+        try {
+            const biodata = await this.biodataService.findMe(userId)
+
+            if (!biodata) throw new NotFoundException('Biodata not found');
+
+            return this.experienceService.update(biodata.id, id, data);
+
+        } catch (error) {
+            throw error;
+
+        }
+    }
+
+    @Roles(Role.Member)
+    @HttpCode(204)
+    @Delete(':id')
+    async remove(@Request() req: any, @Param('id') id: string) {
+        try {
+            const userId = req.user.id
+
+            const biodata = await this.biodataService.findMe(userId)
+
+            if (!biodata) throw new NotFoundException('Biodata not found');
+
+            if (!id) throw new NotFoundException('Id not found');
+            return this.experienceService.remove(biodata.id, id);
+
+        } catch (error) {
+            throw error;
+
+        }
+    }
 }
