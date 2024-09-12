25bf4d7db25c4b836866b3eb435f7820c32e2031
Brian <albashiroh.programming@gmail.com>
2024-09-12 10:05:30 +0700

    partial edit

--- src/activation/activation.controller.ts
+++ src/activation/activation.controller.ts
@@ -8,18 +8,22 @@ import {
     Delete,
 } from '@nestjs/common';
 import { ActivationService } from './activation.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { DeleteActivationDoc, GetAllActivationDoc } from './activation.doc';
 
+@ApiTags('Activations')
+@ApiBearerAuth()
 @Controller('activation')
 export class ActivationController {
     constructor(private readonly activationService: ActivationService) {}
 
-
+    @GetAllActivationDoc()
     @Get()
     findAll() {
         return this.activationService.findAll();
     }
 
-
+    @DeleteActivationDoc()
     @Delete(':id')
     remove(@Param('id') id: string) {
         return this.activationService.remove(+id);
