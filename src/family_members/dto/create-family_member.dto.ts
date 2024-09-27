ccb4d4aa75c2711ceb0f9b762b0c56c6bf191661
Brian <albashiroh.programming@gmail.com>
2024-09-27 09:26:10 +0700

    -create familymember doc -add role restriction to familymember -add 404 exception to a few service

--- src/family_members/dto/create-family_member.dto.ts
+++ src/family_members/dto/create-family_member.dto.ts
@@ -1,22 +1,29 @@
-import { relationship, religion } from "@prisma/client"
-import { IsBoolean, IsEnum, IsString } from "class-validator"
+import { ApiProperty } from '@nestjs/swagger';
+import { relationship, religion } from '@prisma/client';
+import { IsBoolean, IsEnum, IsString } from 'class-validator';
 
 export class CreateFamilyMemberDto {
+    @ApiProperty()
     @IsEnum(relationship)
-    relationship: relationship
+    relationship: relationship;
 
+    @ApiProperty()
     @IsEnum(religion)
-    religion: religion
+    religion: religion;
 
+    @ApiProperty()
     @IsString()
-    dob: string
+    dob: string;
 
+    @ApiProperty()
     @IsString()
-    education: string
+    education: string;
 
+    @ApiProperty()
     @IsString()
-    job: string
+    job: string;
 
+    @ApiProperty()
     @IsBoolean()
-    is_alive: boolean
+    is_alive: boolean;
 }
