18a08c3c263cf8a67823f34172f72fe00a0b7e06
Brian <albashiroh.programming@gmail.com>
2024-09-27 09:56:29 +0700

    add create doc

--- src/galleries/galleries.doc.ts
+++ src/galleries/galleries.doc.ts
@@ -1,4 +1,40 @@
-export function CreateGalleryDoc() {}
+import { applyDecorators } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
+
+export function CreateGalleryDoc() {
+    return applyDecorators(
+        ApiOperation({
+            summary: 'Create Gallery, Restricted for Superadmin & Admin',
+        }),
+        ApiBody({
+            schema: {
+                type: 'object',
+                properties: {
+                    title: {
+                        type: 'string',
+                        example: 'Gallery 1',
+                    },
+                    description: {
+                        type: 'string',
+                        example: 'This is gallery 1',
+                    },
+                    photo: {
+                        type: 'string',
+                        example:
+                            'http://localhost:3000/public/photos/1681681681681_lg.jpg',
+                    },
+                },
+            },
+        }),
+        ApiResponse({
+            status: 201,
+            description: 'Success',
+        }),
+        ApiResponse({
+            
+        })
+    );
+}
 
 export function GetAllGalleriesDoc() {}
 
