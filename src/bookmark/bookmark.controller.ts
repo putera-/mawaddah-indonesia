ce5bedf6bb295212c0e55d1fef7f5abfde475829
Brian <albashiroh.programming@gmail.com>
2024-10-07 09:35:48 +0700

    create bookmark doc

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -11,42 +11,85 @@ import {
 import { BookmarkService } from './bookmark.service';
 import { Roles } from 'src/roles/roles.decorator';
 import { Role } from 'src/roles/role.enums';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+    CheckBookmarkDoc,
+    CreateBookmarkDoc,
+    GetAllBookmarkDoc,
+    GetBookmarkByIdDoc,
+    RemoveBookmarkDoc,
+} from './bookmark.doc';
 
+@ApiTags('Bookmark')
+@ApiBearerAuth()
 @Controller('bookmark')
 export class BookmarkController {
-    constructor(private readonly bookmarkService: BookmarkService) { }
+    constructor(private readonly bookmarkService: BookmarkService) {}
+
+    @CreateBookmarkDoc()
     @Roles(Role.Member)
     @Post(':id')
     create(@Request() req: any, @Param('id') id: string) {
-        const userId = req.user.id;
+        try {
+            const userId = req.user.id;
 
-        return this.bookmarkService.create(userId, id);
+            return this.bookmarkService.create(userId, id);
+        } catch (error) {
+            throw error;
+        }
     }
 
+    @GetAllBookmarkDoc()
     @Roles(Role.Member)
     @Get()
-    findAll(@Request() req: any, @Query('page') page: number, @Query('limit') limit: number) {
-        const id = req.user.id;
-        return this.bookmarkService.findAll(id, page, limit);
+    findAll(
+        @Request() req: any,
+        @Query('page') page: number,
+        @Query('limit') limit: number,
+    ) {
+        try {
+            const id = req.user.id;
+            return this.bookmarkService.findAll(id, page, limit);
+        } catch (error) {
+            throw error;
+        }
     }
 
+    @GetBookmarkByIdDoc()
     @Roles(Role.Member)
     @Get(':id')
     findOne(@Param('id') id: string) {
-        return this.bookmarkService.findOne(id);
+        try {
+            return this.bookmarkService.findOne(id);
+        } catch (error) {
+            throw error;
+        }
     }
 
+    @CheckBookmarkDoc()
     @Roles(Role.Member)
     @Get('check/:id')
-    isBookmarked(@Request() req: any, @Param('id') idCandidate: string): Promise<boolean> {
-        const idUser = req.user.id;
-        return this.bookmarkService.isBookmarked(idUser, idCandidate);
+    isBookmarked(
+        @Request() req: any,
+        @Param('id') idCandidate: string,
+    ): Promise<boolean> {
+        try {
+            const idUser = req.user.id;
+            return this.bookmarkService.isBookmarked(idUser, idCandidate);
+        } catch (error) {
+            throw error;
+        }
     }
 
+    @RemoveBookmarkDoc()
     @Roles(Role.Member)
     @Patch(':id')
     @HttpCode(204)
     remove(@Param('id') id: string) {
-        return this.bookmarkService.remove(id);
+        try {
+            return this.bookmarkService.remove(id);
+        } catch (error) {
+            throw error;
+        }
     }
 }
